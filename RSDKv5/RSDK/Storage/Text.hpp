ec10461036a05f97fffe1099e01e1bb9f06981d9
ikengjj <manuelsteve893@gmail.com>
2025-02-05 12:54:50 -0500

    :trollface:

--- RSDKv5/RSDK/Storage/Text.hpp
+++ RSDKv5/RSDK/Storage/Text.hpp
@@ -1,224 +1,100 @@
-#ifndef TEXT_H
-#define TEXT_H
+#ifndef TEXTSYSTEM_H
+#define TEXTSYSTEM_H
 
-namespace RSDK
-{
+#define TEXTDATA_COUNT  (0x2800)
+#define TEXTENTRY_COUNT (0x200)
+#define TEXTMENU_COUNT  (0x2)
 
-struct GameVersionInfo {
-    char gameTitle[0x40];
-    char gameSubtitle[0x100];
-    char version[0x10];
+#define FONTLIST_CHAR_COUNT (0x1000)
+#define FONTLIST_COUNT      (0x4)
+
+enum TextInfoTypes { TEXTINFO_TEXTDATA = 0, TEXTINFO_TEXTSIZE = 1, TEXTINFO_ROWCOUNT = 2 };
+
+struct TextMenu {
+    ushort textData[TEXTDATA_COUNT];
+    int entryStart[TEXTENTRY_COUNT];
+    int entrySize[TEXTENTRY_COUNT];
+    byte entryHighlight[TEXTENTRY_COUNT];
+    int textDataPos;
+    int selection1;
+    int selection2;
+    ushort rowCount;
+    ushort visibleRowCount;
+    ushort visibleRowOffset;
+    byte alignment;
+    byte selectionCount;
+    sbyte timer;
+};
+
 #if !RETRO_REV02
-    uint8 platform;
-    uint8 language;
-    uint8 region;
+#define FONTCHAR_COUNT (0x400)
+
+struct FontCharacter {
+    int id;
+    short srcX;
+    short srcY;
+    short width;
+    short height;
+    short pivotX;
+    short pivotY;
+    short xAdvance;
+};
 #endif
+
+struct BitmapFontCharacter {
+    ushort id;
+    float x;
+    float y;
+    float width;
+    float height;
+    float xOffset;
+    float yOffset;
+    float xAdvance;
+    ushort textureID;
 };
 
-extern GameVersionInfo gameVerInfo;
-
-struct String {
-    uint16 *chars; // text
-    uint16 length;  // string length
-    uint16 size;    // total alloc length
+struct BitmapFont {
+    BitmapFontCharacter characters[FONTLIST_CHAR_COUNT];
+    ushort count;
+    float lineHeight;
+    float base;
 };
 
-#if RETRO_REV0U
-inline void StrCopy(char *dest, const char *src)
-{
-#ifdef USE_STDLIB
-    strcpy(dest, src);
-#else
-    int32 i = 0;
-    for (; src[i]; ++i) dest[i] = src[i];
-    dest[i] = 0;
-#endif
-}
+enum TextMenuAlignments {
+    MENU_ALIGN_LEFT,
+    MENU_ALIGN_RIGHT,
+    MENU_ALIGN_CENTER,
+};
 
-inline void StrAdd(char *dest, const char *src)
-{
-    int32 destStrPos = 0;
-    int32 srcStrPos  = 0;
-    while (dest[destStrPos]) ++destStrPos;
-    while (true) {
-        if (!src[srcStrPos]) {
-            break;
-        }
-        dest[destStrPos++] = src[srcStrPos++];
-    }
-    dest[destStrPos] = 0;
-}
+extern TextMenu gameMenu[TEXTMENU_COUNT];
+extern int textMenuSurfaceNo;
 
-inline bool StrComp(const char *stringA, const char *stringB)
-{
-    bool32 match    = true;
-    bool32 finished = false;
-    while (!finished) {
-        if (*stringA == *stringB || *stringA == *stringB + ' ' || *stringA == *stringB - ' ') {
-            if (*stringA) {
-                ++stringA;
-                ++stringB;
-            }
-            else {
-                finished = true;
-            }
-        }
-        else {
-            match    = false;
-            finished = true;
-        }
-    }
+extern char playerListText[0x80][0x20];
 
-    return match;
-}
+extern BitmapFont fontList[FONTLIST_COUNT];
 
-inline int32 StrLength(const char *string)
-{
-    int32 len = 0;
-    for (len = 0; string[len]; len++)
-        ;
-    return len;
-}
+#if !RETRO_REV02
+extern FontCharacter fontCharacterList[FONTCHAR_COUNT];
 
-int32 FindStringToken(const char *string, const char *token, uint8 stopID);
+void LoadFontFile(const char *filePath);
 #endif
 
-inline void StringLowerCase(char *dest, const char *src)
-{
-    int32 destPos = 0;
-    int32 curChar = *src;
-    if (*src) {
-        int32 srcPos = 0;
-        do {
-            while ((uint32)curChar - 'A' < 26) {
-                destPos       = srcPos;
-                dest[destPos] = curChar + ' ';
-                curChar       = src[++srcPos];
-                if (!curChar) {
-                    dest[++destPos] = 0;
-                    return;
-                }
-            }
-            destPos       = srcPos;
-            dest[destPos] = curChar;
-            curChar       = src[++srcPos];
-        } while (curChar);
-    }
-    dest[++destPos] = 0;
-}
+void LoadTextFile(TextMenu *menu, const char *filePath, byte mapCode);
+void LoadConfigListText(TextMenu *menu, int listNo);
 
-inline void StringUpperCase(char *dest, const char *src)
-{
-    int32 destPos = 0;
-    int32 curChar = *src;
-    if (*src) {
-        int32 srcPos = 0;
-        do {
-            while ((uint32)curChar - 'a' < 26) {
-                destPos       = srcPos;
-                dest[destPos] = curChar - ' ';
-                curChar       = src[++srcPos];
-                if (!curChar) {
-                    dest[++destPos] = 0;
-                    return;
-                }
-            }
-            destPos       = srcPos;
-            dest[destPos] = curChar;
-            curChar       = src[++srcPos];
-        } while (curChar);
-    }
-    dest[++destPos] = 0;
-}
+void SetupTextMenu(TextMenu *menu, int rowCount);
+void AddTextMenuEntry(TextMenu *menu, const char *text);
+void AddTextMenuEntryW(TextMenu *menu, const ushort *text);
+void SetTextMenuEntry(TextMenu *menu, const char *text, int rowID);
+void SetTextMenuEntryW(TextMenu *menu, const ushort *text, int rowID);
+void EditTextMenuEntry(TextMenu *menu, const char *text, int rowID);
 
-extern char textBuffer[0x400];
-void GenerateHashMD5(uint32 *buffer, char *textBuffer, int32 textBufferLen);
-void GenerateHashCRC(uint32 *id, char *inputString);
+void LoadBitmapFont(const char *filePath, int index, char textureID);
+void ResetBitmapFonts();
+float GetTextWidth(ushort *text, int fontID, float scaleX);
+float GetTextHeight(ushort *text, int fontID, float scaleY);
+void SetStringToFont(ushort *text, ushort *string, int fontID);
+void SetStringToFont8(ushort *text, const char *string, int fontID);
+void AddTimeStringToFont(ushort *text, int time, int fontID);
 
-#define RETRO_HASH_MD5(name) uint32 name[4]
-#define HASH_SIZE_MD5        (4 * sizeof(uint32))
-#define HASH_MATCH_MD5(a, b) (memcmp(a, b, HASH_SIZE_MD5) == 0)
-// this is NOT thread-safe!
-#define GEN_HASH_MD5(text, hash)                                                                                                                     \
-    strcpy(textBuffer, text);                                                                                                                        \
-    GenerateHashMD5(hash, textBuffer, (int32)strlen(textBuffer))
-// this one is but assumes buffer has already been setup
-#define GEN_HASH_MD5_BUFFER(buffer, hash) GenerateHashMD5(hash, buffer, (int32)strlen(buffer))
-#define HASH_COPY_MD5(dst, src) memcpy(dst, src, HASH_SIZE_MD5)
-#define HASH_CLEAR_MD5(hash)    MEM_ZERO(hash)
-
-inline void InitString(String *string, const char *text, uint32 textLength)
-{
-    string->length = 0;
-    string->size = 0;
-
-    if (text != NULL) {
-        string->length = 0;
-        while (text[string->length] != '\0') ++string->length;
-
-        if (textLength != 0 && textLength >= string->length)
-            string->size = textLength;
-        else
-            string->size = string->length;
-
-        if (string->size == 0)
-            string->size = 1;
-
-        AllocateStorage((void **)&string->chars, sizeof(uint16) * string->size, DATASET_STR, false);
-
-        for (uint32 pos = 0; text[pos] != '\0'; ++pos)
-            string->chars[pos] = text[pos];
-    }
-}
-
-void SetString(String *string, const char *text);
-
-inline void CopyString(String *dst, String *src)
-{
-    if (dst == src)
-        return;
-
-    int32 srcLength = src->length;
-    dst->chars      = NULL;
-    if (dst->size >= srcLength) {
-        if (!dst->chars) {
-            AllocateStorage((void **)&dst->chars, sizeof(uint16) * dst->size, DATASET_STR, false);
-        }
-    }
-    else {
-        dst->size = srcLength;
-        AllocateStorage((void **)&dst->chars, sizeof(uint16) * dst->size, DATASET_STR, false);
-    }
-
-    dst->length = src->length;
-    for (int32 c = 0; c < dst->length; ++c) dst->chars[c] = src->chars[c];
-}
-
-inline void GetCString(char *destChars, String *string)
-{
-    if (!string->chars)
-        return;
-
-    char *cString = destChars ? destChars : textBuffer;
-    int32 textLen = destChars ? string->length : 0x400;
-
-    int32 c = 0;
-    for (; c < textLen; ++c) cString[c] = (char)string->chars[c];
-    cString[c] = 0;
-}
-
-void AppendText(String *string, const char *appendText);
-void AppendString(String *string, String *appendString);
-bool32 CompareStrings(String *string1, String *string2, bool32 exactMatch);
-
-void InitStringList(String *stringList, int32 size);
-void LoadStringList(String *stringList, const char *filePath, uint32 charSize);
-bool32 SplitStringList(String *splitStrings, String *stringList, int32 startStringID, int32 stringCount);
-
-#if RETRO_REV0U
-#include "Legacy/TextLegacy.hpp"
-#endif
-
-} // namespace RSDK
-
-#endif
+#endif // !TEXTSYSTEM_H
